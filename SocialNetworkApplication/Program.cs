5f0540170aad87b181603772a7cb5a046d6d1c7d
Berthelmaster <Berthel0802@gmail.com>
2019-05-07 15:11:25 +0200

    test

--- SocialNetworkApplication/Program.cs
+++ SocialNetworkApplication/Program.cs
@@ -20,5 +20,6 @@ namespace SocialNetworkApplication
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+        // Test
     }
 }
