9ff8e199c859710d0991bd6956c369fd436938b8
Thomas Niels Berthelsen <au592332@uni.au.dk>
2019-05-07 14:30:23 +0200

    Added project, Nosql

---
+++ SocialNetworkApplication/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetworkApplication.Model
+{
+    public class User
+    {
+        [BsonId]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public int Age { get; set; }
+    }
+}
