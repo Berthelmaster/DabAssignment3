856fd199a234c692736888ba1b053d25585496ed
Berthelmaster <Berthel0802@gmail.com>
2019-05-14 16:33:59 +0200

    hh

--- SocialNetworkApplication/Controller/CircleController.cs
+++ SocialNetworkApplication/Controller/CircleController.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetworkApplication.Model;
+using SocialNetworkApplication.Services;
 
 namespace SocialNetworkApplication.Controller
 {
     public class CircleController
     {
+
+
+        private readonly CircleService _circleService;
+
+        public CircleController(CircleService circleService)
+        {
+            _circleService = circleService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Circle>> Get()
+        {
+            return _circleService.Get();
+        }
+
+        [HttpGet("{id:length(24)}", Name = "getName")]
+        public ActionResult<Circle> Get
+
+
     }
 }
