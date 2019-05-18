8507a0c00e3e237b42a17f82495fa14f80cfe405
Berthelmaster <Berthel0802@gmail.com>
2019-05-18 11:53:09 +0200

    dummy push

--- SocialNetworkApplication/Startup.cs
+++ SocialNetworkApplication/Startup.cs
@@ -7,12 +7,18 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
+
 namespace SocialNetworkApplication
 {
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup()
+        {
+            
+            
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<Services.UserService>();
