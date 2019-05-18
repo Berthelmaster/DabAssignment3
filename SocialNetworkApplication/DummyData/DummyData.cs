d5d6b5372755b53b2c7949fbe206f22ec6f73601
Berthelmaster <Berthel0802@gmail.com>
2019-05-18 12:48:24 +0200

    pushed

--- SocialNetworkApplication/DummyData/DummyData.cs
+++ SocialNetworkApplication/DummyData/DummyData.cs
@@ -16,16 +16,17 @@ namespace SocialNetworkApplication.DummyData
     {
         private readonly IMongoCollection<User> _user;
         private readonly IMongoCollection<Post> _post;
-        private readonly IMongoCollection<Post> _comment;
+        private readonly IMongoCollection<Comment> _comment;
         private readonly IMongoCollection<Circle> _circle;
 
-        public DummyData(IConfiguration config)
+        public DummyData()
         {
-            var client = new MongoClient(config.GetConnectionString("SocialMediaDb"));
-            var database = client.GetDatabase("SocialMediaDb");
+            var connectionstring = "SocialNetworkPlatform";
+            var client = new MongoClient(connectionstring);
+            var database = client.GetDatabase(connectionstring);
             _user = database.GetCollection<User>("User");
             _post = database.GetCollection<Post>("Post");
-            _comment = database.GetCollection<Post>("Comment");
+            _comment = database.GetCollection<Comment>("Comment");
             _circle = database.GetCollection<Circle>("Circle");
 
             UserDummy(_user);
@@ -44,36 +45,34 @@ namespace SocialNetworkApplication.DummyData
                     Id              = "00001",
                     Name            = "Hans",
                     Gender          = "Male",
-                    Age             = "26",
+                    Age             = 26,
                     BlockedList     = new List<string> {"00001"},
-                    Followers       = new List<string> {"00001"},
-                    Following       = new List<string> {"00001"},
+                    Followings      = new List<string> {"00001"},
+                    Follows         = new List<string> {"00001"},
                     Circles         = new List<string> {"00001"},
-                    Posts           = new List<string> {"00001"}
+                   
                 },
                 new User
                 {
                     Id              = "00002",
                     Name            = "Anne",
                     Gender          = "Female",
-                    Age             = "25",
+                    Age             = 25,
                     BlockedList     = new List<string> {"00002"},
-                    Followers       = new List<string> {"00002"},
-                    Following       = new List<string> {"00002"},
-                    Circles         = new List<string> {"00002"},
-                    Posts           = new List<string> {"00002"}
+                    Followings      = new List<string> {"00002"},
+                    Follows         = new List<string> {"00002"},
+                    Circles         = new List<string> {"00002"}
                 },
                 new User
                 {
                     Id              = "00003",
                     Name            = "Mads",
                     Gender          = "male",
-                    Age             = "27",
+                    Age             = 27,
                     BlockedList     = new List<string> {"00003"},
-                    Followers       = new List<string> {"00003"},
-                    Following       = new List<string> {"00003"},
+                    Followings      = new List<string> {"00003"},
+                    Follows         = new List<string> {"00003"},
                     Circles         = new List<string> {"00003"},
-                    Posts           = new List<string> {"00003"}
                 }
             };
             await user.InsertManyAsync(users);
@@ -85,94 +84,94 @@ namespace SocialNetworkApplication.DummyData
                 new Post
                 {
                     Id              = "00001",
-                    Circle          = "00001",
+                    Circle          =  new Circle(),
                     TextContent     = "Post 1",
                     ImageContent    = null,
                     Privacy         = "Public",
-                    Autoher         = "00001",
-                    CreationTime    = "10/05/2018",
-                    Comments        = new List<string> {"00001"}
+                    Author          = "00001",
+                    CreationTime    = DateTime.Today,
+                    Comments        = new List<Comment>()
                 },
                 new Post
                 {
                     Id              = "00002",
-                    Circle          = "00002",
+                    Circle          = new Circle(),
                     TextContent     = "Post 2",
                     ImageContent    = null,
                     Privacy         = "Public",
-                    Autoher         = "00002",
-                    CreationTime    = "11/05/2018",
-                    Comments        = new List<string> {"00002"}
+                    Author          = "00002",
+                    CreationTime    = DateTime.Now,
+                    Comments        = new List<Comment>()
                 },
                 new Post
                 {
                     Id              = "00003",
-                    Circle          = "00003",
+                    Circle          = new Circle(),
                     TextContent     = "Post 3",
                     ImageContent    = null,
                     Privacy         = "Public",
-                    Autoher         = "00003",
-                    CreationTime    = "12/05/2018",
-                    Comments        = new List<string> {"00003"}
+                    Author          = "00003",
+                    CreationTime    = DateTime.MaxValue,
+                    Comments        = new List<Comment>()
                 }
             };
-            await user.InsertManyAsync(users);
+            await post.InsertManyAsync(posts);
         }
         static async void CircleDummy(IMongoCollection<Circle> circle)
         {
-            var circle = new List<Post>
+            var circles = new List<Circle>
             {
                 new Circle
                 {
                     Id              = "00001",
                     Name            = "Main Circle",
-                    Posts           = new List<string> {"00001"},
-                    User            = new List<string> {"00001"}
+                    Posts           = new List<Post>(),
+                    Users           = new List<string> {"00001"}
                 },
                 new Circle
                 {
                     Id              = "00002",
                     Name            = "Work",
-                    Posts           = new List<string> {"00002"},
-                    User            = new List<string> {"00002"}
+                    Posts           = new List<Post>(),
+                    Users            = new List<string> {"00002"}
                 },
                 new Circle
                 {
                     Id              = "00003",
                     Name            = "School",
-                    Posts           = new List<string> {"00003"},
-                    User            = new List<string> {"00003"}
+                    Posts           = new List<Post>(),
+                    Users            = new List<string> {"00003"}
                 }
             };
-            await user.InsertManyAsync(users);
+            await circle.InsertManyAsync(circles);
         }
         static async void CommentDummy(IMongoCollection<Comment> comment)
         {
-            var comment = new List<Post>
+            var comments = new List<Comment>
             {
                 new Comment
                 {
                     Id              = "00001",
-                    Name            = "00001",
+                    Author          = "00001",
                     Text            = "Nice",
-                    CreationTime    = "10/05/2018"
+                    CreationTime    = new DateTime(2019, 1, 19)
                 },
                 new Comment
                 {
                     Id              = "00002",
-                    Name            = "00002",
+                    Author          = "00002",
                     Text            = "Cool",
-                    CreationTime    = "11/05/2018"
+                    CreationTime    = DateTime.MinValue,
                 },
                 new Comment
                 {
                     Id              = "00003",
-                    Name            = "00003",
+                    Author          = "00003",
                     Text            = "Amazing",
-                    CreationTime    = "12/05/2018"
+                    CreationTime    = new DateTime(1990, 12, 12),
                 }
             };
-            await user.InsertManyAsync(users);
+            await comment.InsertManyAsync(comments);
         }
     }
 }
